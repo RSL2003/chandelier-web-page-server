6eb8b1c27ea18da74ffe23bf585fd4c264b38316
RSL <rsl@venomcraft.win>
2021-03-09 18:33:51 -0400

    Login issues, Do NOT USE

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
 from flask import Flask, render_template
 
+from config import Config
+
 app = Flask(__name__)
+app.config.from_object(Config)
 
 
+# define your app routes
 @app.route('/')
+
 @app.route('/index')
 def index():
     page = {'pagename': 'Start Page'}
     return render_template('index.html', title='home', page=page)
-
-
+@app.route('/login')
+def login():
+    page = {'pagename': 'login'}
+    form = LoginForm
+    return render_template('login.html', title='Login', page=page)
 if __name__ == '__main__':
     app.run(debug=True)
