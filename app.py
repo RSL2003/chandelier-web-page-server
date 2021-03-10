eb645f23230cf71c68a636b267dc2216d028bea0
RSL <rsl@venomcraft.win>
2021-03-09 20:02:28 -0400

    comments added for issues, adding more formatted login page later

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
+from forms import LoginForm # issues with this import still
 from flask import Flask, render_template
+
 
 from config import Config
 
@@ -8,15 +10,18 @@ app.config.from_object(Config)
 
 # define your app routes
 @app.route('/')
-
 @app.route('/index')
 def index():
     page = {'pagename': 'Start Page'}
     return render_template('index.html', title='home', page=page)
-@app.route('/login')
+
+
+@app.route('/login')   # still has issues with loading
 def login():
     page = {'pagename': 'login'}
     form = LoginForm
-    return render_template('login.html', title='Login', page=page)
+    return render_template('login.html', title='Login', page=page, form=form)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
