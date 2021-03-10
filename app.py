e01e371b70d9d211f9b7f23860f36468462e027e
RSL <rsl@venomcraft.win>
2021-03-09 22:45:19 -0400

    Finished working on login.html. Yuval to work on database for users. Start of css.

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from forms import LoginForm
 from flask import Flask, render_template
-
-
+from flask import render_template, flash, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy  # prob wont use in final version
+from flask_migrate import Migrate  # most likely wont use in final version
 from config import Config
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config.from_object(Config)
-
+app.config.from_object(Config)
+bootstrap = Bootstrap(app)
 
 # define your app routes
 @app.route('/')
@@ -16,10 +19,14 @@ def index():
     return render_template('/html/index.html', title='home', page=page)
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     page = {'pagename': 'login'}
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
     return render_template('/html/login.html', title='Login', page=page, form=form)
 
 
