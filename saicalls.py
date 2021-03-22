dfa7c5cb5d19ff58f90808b1f7d52faab1f9eb6b
Jared <jmanmr2003@gmail.com>
2021-03-22 11:18:01 -0300

    added small ammount of comments to saicalls.py to explain how we will be setting functions.

--- saicalls.py
+++ saicalls.py
@@ -1,5 +1,12 @@
 # this is just a test function
 
+# for using serial port
+# 1 define the device being talked to.
+# 2 define the port to be used by the pi. ex com1
+# 3 Send command to the sai
+# 4 close port
+# 5 listen to the device on com 2 for any errors back. byte byte ‘199’ (0xC7), following unknowen command.
+
 def blinkybeinky():
     print('this works')
 
